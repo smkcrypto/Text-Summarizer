be8d39e8176d980d4566a7edce5532ea2cd48aae
Mohan <sampathraomohankumar@gmail.com>
2024-11-06 13:29:14 +0530

    some changes, logging file and setup

--- template.py
+++ template.py
@@ -11,8 +11,8 @@ list_of_files=[
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
-    f"src/{project_name}/logging/init.py",
-    f"src/{project_name}/config/init.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
@@ -35,7 +35,7 @@ for filepath in list_of_files:
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory:{filedir} for the file {filename}")
 
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==o):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
         with open(filepath,'w') as f:
             pass
         logging.info(f"creating empty file: {filepath}")
